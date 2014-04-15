15a6a5802e3e75b06a392c952f8f4feb16652203
Erik Waingarten <erikwaing@gmail.com>
2014-04-15 00:15:34 -0400

    i think the ilp is working

--- Code/GenerateILP.py
+++ Code/GenerateILP.py
@@ -16,7 +16,7 @@ This will just be all the variables. We return a vector where the variables are 
 the dictionary order
 '''
 def generateOptimization(points):
-	return [-1 for i in len(points)*len(points)]
+	return [-1 for i in range(len(points)*len(points))]
 
 def generateConstraints(points):
 	firstType = generateFirstContraints(points)
@@ -33,12 +33,12 @@ def generateFirstContraints(points):
 	constraintsLeft = []
 	constraintsRight = []
 	for point in points:
-		lessThanOrEqualTo = [0 for i in len(points)*len(points)]
+		lessThanOrEqualTo = [0 for i in range(len(points)*len(points))]
 		lessThanOrEqualTo[point[0]*len(points) + point[1]] = 1
 		constraintsLeft.append(lessThanOrEqualTo)
 		constraintsRight.append(1)
 
-		greaterThanOrEqualTo = [0 for i in len(points)*len(points)]
+		greaterThanOrEqualTo = [0 for i in range(len(points)*len(points))]
 		greaterThanOrEqualTo[point[0]*len(points) + point[1]] = -1
 		constraintsLeft.append(greaterThanOrEqualTo)
 		constraintsRight.append(-1)
@@ -47,27 +47,40 @@ def generateFirstContraints(points):
 '''
 These contraints say that each rectangle is satified
 '''
-def generateSecondConstaints(points):
+def generateSecondConstraints(points):
 	n = len(points)
 	constraintsLeft = []
 	constraintsRight = []
 	for i in range(n):
 		for j in range(n):
-			for x in range(n):
-				for y in range(n):
+			for x in range(i, n):
+				for y in range(j, n):
 					one = [i,j]
 					two = [x,y]
 					if abs((one[0]-two[0])*(one[1]-two[1])) > 0: # there is an area between the two points
-						constraint = [0 for k in n*n]
-						for l in range(one[0], two[0]+1)+range(two[0], one[0]+1):
-							constraint[l*n + one[1]] = -1
-							constraint[l*n + two[1]] = -1
-						for l in range(one[1], two[1]+1)+range(two[1], one[1]+1):
-							constraint[one[0]*n + l] = -1
-							constraint[two[1]*n + l] = -1
-						constraint[i*n + j] = 1
-						constraint[x*n + y] = 1
-						constraintsLeft.append(contraint)
+						constraintPos = [0 for k in range(n*n)]
+						constraintNeg = [0 for k in range(n*n)]
+						for l in range(one[0]+1, two[0]):
+							constraintPos[l*n + one[1]] = -1
+							constraintPos[l*n + two[1]] = -1
+							constraintNeg[l*n + one[1]] = -1
+							constraintNeg[l*n + two[1]] = -1
+						for l in range(one[1]+1, two[1]):
+							constraintPos[one[0]*n + l] = -1
+							constraintPos[two[1]*n + l] = -1
+							constraintNeg[one[0]*n + l] = -1
+							constraintNeg[two[1]*n + l] = -1
+						constraintPos[i*n + y] = -1
+						constraintPos[x*n + j] = -1
+						constraintPos[i*n + j] = 1
+						constraintPos[x*n + y] = 1
+						constraintNeg[i*n + y] = 1
+						constraintNeg[x*n + j] = 1
+						constraintNeg[i*n + j] = -1
+						constraintNeg[x*n + y] = -1
+						constraintsLeft.append(constraintPos)
+						constraintsRight.append(1)
+						constraintsLeft.append(constraintNeg)
 						constraintsRight.append(1)
 	return constraintsLeft, constraintsRight
 
--- Code/SolveLP.py
+++ Code/SolveLP.py
@@ -5,9 +5,22 @@ def solve(points):
 	lp = generateILP(points)
 	maximizationOptimization = lp[0]
 	constraints = lp[1]
-	problem = inputProblemIntoPulp(maximizationOptimization, constraints)
-	GLPK().solve(problem)
+	problem, variables = inputProblemIntoPulp(maximizationOptimization, constraints)
+	problem.solve()
+	for i in range(len(maximizationOptimization)):
+		print "Point (", i / len(points), ",", i % len(points), "): ", variables[i].varValue 
+
 
 def inputProblemIntoPulp(optimization, constraints):
 	problem = LpProblem("Points in Plane", LpMaximize)
-	
+	variableIndexes = range(len(optimization))
+	variables = LpVariable.dicts("Points", variableIndexes, 0, 1, LpBinary)
+	problem += lpSum(optimization[i]*variables[i] for i in variableIndexes)
+	print "Left: ", constraints[0]
+	print "Right: ", constraints[1]
+	for i, left in enumerate(constraints[0]):
+		problem += lpSum(variables[i]*left[i] for i in variableIndexes) <= constraints[1][i]
+	return problem, variables
+
+points = [(0,0), (1, 1), (2,2), (3, 3), (4, 4), (5,5)]
+solve(points)
